45ba966650a3cafe039b16527e6258695d6195f6
David Mendoza <d.mendoza99@unitec.edu>
2018-03-29 17:42:57 -0600

    Arbol General
    Cree la clase nodo y el arbol :v

--- Proyecto_EDDI/src/proyecto_eddi/Tree.java
+++ Proyecto_EDDI/src/proyecto_eddi/Tree.java
@@ -17,6 +17,10 @@ public class Tree {
         this.Root = null;
     }
 
+    public Tree(TreeNode Root) {
+        this.Root = Root;
+    }
+
     public TreeNode getRoot() {
         return Root;
     }
@@ -24,8 +28,72 @@ public class Tree {
     public void setRoot(TreeNode Root) {
         this.Root = Root;
     }
-    /*
-    YA NO SE QUE HACER
-     */
 
+    public void clear() {
+        this.Root = null;
+    }
+
+    public TreeNode addChild(TreeNode child) {
+        child.setParent(this.Root);
+        this.Root.getChildren().add(child);
+        return child;
+    }
+
+    public void addChildren(ArrayList<TreeNode> children) {
+        for (int i = 0; i < children.length(); i++) {
+            this.Root.getChildren().get(i).setParent(this.Root);
+        }
+
+        for (int i = 0; i < children.length(); i++) {
+            this.Root.getChildren().add(children.get(i));
+        }
+    }
+
+    public void printTree(TreeNode node, String appender) {
+        System.out.println(appender + node.getData());
+        for (int i = 0; i < this.Root.getChildren().length(); i++) {
+            this.Root.getChildren().get(i).printTree(this.Root.getChildren().get(i), appender + appender);
+        }
+    }
+
+    public void CalificarNodo(TreeNode node) {
+        if (!node.isLeaf(node)) {
+            node.getData().setCalificacion(node.CalificarHijos(node, 0) / node.getChildren().length());
+        }
+        for (int i = 0; i < this.Root.getChildren().length(); i++) {
+            this.Root.getChildren().get(i).CalificarNodo(node);
+        }
+    }
+
+    public boolean isLeaf(TreeNode node) {
+        return (node.getChildren().length() == 0);
+    }
+
+    public float CalificarHijos(TreeNode node, float num) {
+        if (node.isLeaf(node)) {
+            return node.getData().getCalificacion();
+        } else {
+            for (int i = 0; i < node.getChildren().length(); i++) {
+                num += node.getChildren().get(i).getData().getCalificacion();
+                node.getData().setCalificacion(num);
+            }
+        }
+        return node.getData().getCalificacion();
+    }
+
+    public static void main(String[] args) {
+        TreeNode CEO = new TreeNode(new Empleado(90, "Josue", "Ceo"));
+        TreeNode Chief = new TreeNode(new Empleado(100, "David", "CHIEF PROGRAMMER"));
+        TreeNode Chief2 = new TreeNode(new Empleado(50, "Iliana", "CHIEF PROGRAMMER"));
+        TreeNode Progra = new TreeNode(new Empleado(60, "gato", "programador"));
+        TreeNode Progra2 = new TreeNode(new Empleado(60, "Perro", "programador"));
+        Tree t = new Tree(CEO);
+        t.addChild(Chief);
+        t.addChild(Chief2);
+        Chief.addChild(Progra);
+        Chief.addChild(Progra2);
+        Chief.CalificarNodo(Chief);
+        t.CalificarNodo(t.getRoot());
+        t.printTree(CEO, "-");
+    }
 }
--- Proyecto_EDDI/src/proyecto_eddi/TreeNode.java
+++ Proyecto_EDDI/src/proyecto_eddi/TreeNode.java
@@ -11,28 +11,80 @@ package proyecto_eddi;
  */
 public class TreeNode {
 
-    private Empleado empleado;
-    private ArrayList<TreeNode> hijos;
+    private Empleado data = null;
+    private final ArrayList<TreeNode> children = new ArrayList<>();
+    private TreeNode parent = null;
 
-    public TreeNode(Empleado empleado) {
-        this.empleado = empleado;
-        this.hijos = new ArrayList();
+    public TreeNode(Empleado data) {
+        this.data = data;
     }
 
-    public Empleado getEmpleado() {
-        return empleado;
+    public TreeNode addChild(TreeNode child) {
+        child.setParent(this);
+        this.children.add(child);
+        return child;
     }
 
-    public void setEmpleado(Empleado empleado) {
-        this.empleado = empleado;
+    public void addChildren(ArrayList<TreeNode> children) {
+        for (int i = 0; i < children.length(); i++) {
+            this.children.get(i).setParent(this);
+        }
+
+        for (int i = 0; i < children.length(); i++) {
+            this.children.add(children.get(i));
+        }
     }
 
-    public ArrayList<TreeNode> getHijos() {
-        return hijos;
+    public ArrayList<TreeNode> getChildren() {
+        return children;
     }
 
-    public void setHijos(ArrayList<TreeNode> hijos) {
-        this.hijos = hijos;
+    public Empleado getData() {
+        return data;
     }
 
+    public void setData(Empleado data) {
+        this.data = data;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void printTree(TreeNode node, String appender) {
+        System.out.println(appender + node.getData());
+        for (int i = 0; i < this.children.length(); i++) {
+            this.children.get(i).printTree(this.children.get(i), appender + appender);
+        }
+    }
+
+    public void CalificarNodo(TreeNode node) {
+        if (!node.isLeaf(node)) {
+            node.getData().setCalificacion(node.CalificarHijos(node, 0) / node.getChildren().length());
+        }
+        for (int i = 0; i < this.children.length(); i++) {
+            this.children.get(i).CalificarNodo(node);
+        }
+    }
+
+    public boolean isLeaf(TreeNode node) {
+        return (node.getChildren().length() == 0);
+    }
+
+    public float CalificarHijos(TreeNode node, float num) {
+
+        if (node.isLeaf(node)) {
+            return node.getData().getCalificacion();
+        } else {
+            for (int i = 0; i < node.getChildren().length(); i++) {
+                num += node.getChildren().get(i).getData().getCalificacion();
+                node.getData().setCalificacion(num);
+            }
+        }
+        return node.getData().getCalificacion();
+    }
 }
